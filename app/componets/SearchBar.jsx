8b3758cee19557a74c40c7c86bacb8cc51ab3416
javilafoure <javilafoure@gmail.com>
2024-11-13 23:37:52 -0500

    add structure and searchbar

---
+++ app/componets/SearchBar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div className="bg-slate-500 shadow-md shadow-slate-700 w-[70%] h-10 rounded-full flex items-center justify-between px-4 cursor-pointer">
+      <input
+        className="bg-slate-500 justify-center focus:outline-none"
+        placeholder="Buscar"
+        type="text"
+      />
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        strokeWidth={1.5}
+        stroke="currentColor"
+        className="size-6 text-slate-700"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+        />
+      </svg>
+    </div>
+  );
+}
+
+export default SearchBar;
---
+++ app/page.jsx
@@ -0,0 +1,8 @@
+import SearchBar from "./componets/SearchBar";
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center py-5">
+      <SearchBar />
+    </div>
+  );
+}
